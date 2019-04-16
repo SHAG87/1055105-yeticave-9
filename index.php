65f7316ad542f89b2153e5879a7379281f528208
Andrey <shevlyakov-a@mail.ru>
2019-04-16 13:15:43 +0300

    Use PHP Style

--- index.php
+++ index.php
@@ -1,43 +1,43 @@
 <?php
 $is_auth = rand(0, 1);
 $user_name = 'Andrey';
-$cats=["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
-$announ=[
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+$announ = [
     [
-        'name'=>'2014 Rossignol District Snowboard',
-        'cat'=>'Доски и лыжи',
-        'price'=>10999,
-        'url'=>'img/lot-1.jpg'
+        'name' => '2014 Rossignol District Snowboard',
+        'cat' => 'Доски и лыжи',
+        'price' => 10999,
+        'url' => 'img/lot-1.jpg'
     ],
     [
-        'name'=>'DC Ply Mens 2016/2017 Snowboard',
-        'cat'=>'Доски и лыжи',
-        'price'=>159999,
-        'url'=>'img/lot-2.jpg'
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'cat' => 'Доски и лыжи',
+        'price' => 159999,
+        'url' => 'img/lot-2.jpg'
     ],
     [
-        'name'=>'Крепления Union Contact Pro 2015 года размер L/XL',
-        'cat'=>'Крепления',
-        'price'=>8000,
-        'url'=>'img/lot-3.jpg'
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'cat' => 'Крепления',
+        'price' => 8000,
+        'url' => 'img/lot-3.jpg'
     ],
     [
-        'name'=>'Ботинки для сноуборда DC Mutiny Charocal',
-        'cat'=>'Ботинки',
-        'price'=>10999,
-        'url'=>'img/lot-4.jpg'
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'cat' => 'Ботинки',
+        'price' => 10999,
+        'url' => 'img/lot-4.jpg'
     ],
     [
-        'name'=>'Куртка для сноуборда DC Mutiny Charocal',
-        'cat'=>'Одежда',
-        'price'=>7500,
-        'url'=>'img/lot-5.jpg'
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'cat' => 'Одежда',
+        'price' => 7500,
+        'url' => 'img/lot-5.jpg'
     ],
     [
-        'name'=>'Маска Oakley Canopy',
-        'cat'=>'Разное',
-        'price'=>5400,
-        'url'=>'img/lot-6.jpg'
+        'name' => 'Маска Oakley Canopy',
+        'cat' => 'Разное',
+        'price' => 5400,
+        'url' => 'img/lot-6.jpg'
     ]
 ];
 ?>
@@ -92,8 +92,8 @@ $announ=[
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <!--заполните этот список из массива категорий-->
-            <?php foreach ($cats as $key=>$val):?>
+            <!--Выводим список категорий $categories-->
+            <?php foreach ($categories as $key=>$val):?>
             <li class="promo__item promo__item--boards">
                 <a class="promo__link" href="pages/all-lots.html"><?=$val?></a>
             </li>
@@ -134,8 +134,8 @@ $announ=[
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполните этот список из массива категорий-->
-            <?php foreach ($cats as $key=>$val):?>
+            <!--Выводим категории списком-->
+            <?php foreach ($categories as $key=>$val):?>
             <li class="nav__item">
                 <a href="pages/all-lots.html"><?=$val?></a>
             </li>
