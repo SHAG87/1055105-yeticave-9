68a73144ddc43b4cf12753de080401ae1559cf7f
Andrey <shevlyakov-a@mail.ru>
2019-04-16 13:28:05 +0300

    Modify function format_sum

--- index.php
+++ index.php
@@ -1,54 +1,50 @@
 <?php
 $is_auth = rand(0, 1);
 $user_name = 'Andrey';
-$cats=["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
-$announ=[
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+$announ = [
     [
-        'name'=>'2014 Rossignol District Snowboard',
-        'cat'=>'Доски и лыжи',
-        'price'=>10999,
-        'url'=>'img/lot-1.jpg'
+        'name' => '2014 Rossignol District Snowboard',
+        'cat' => 'Доски и лыжи',
+        'price' => 10999,
+        'url' => 'img/lot-1.jpg'
     ],
     [
-        'name'=>'DC Ply Mens 2016/2017 Snowboard',
-        'cat'=>'Доски и лыжи',
-        'price'=>159999,
-        'url'=>'img/lot-2.jpg'
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'cat' => 'Доски и лыжи',
+        'price' => 159999,
+        'url' => 'img/lot-2.jpg'
     ],
     [
-        'name'=>'Крепления Union Contact Pro 2015 года размер L/XL',
-        'cat'=>'Крепления',
-        'price'=>8000,
-        'url'=>'img/lot-3.jpg'
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'cat' => 'Крепления',
+        'price' => 8000,
+        'url' => 'img/lot-3.jpg'
     ],
     [
-        'name'=>'Ботинки для сноуборда DC Mutiny Charocal',
-        'cat'=>'Ботинки',
-        'price'=>10999,
-        'url'=>'img/lot-4.jpg'
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'cat' => 'Ботинки',
+        'price' => 10999,
+        'url' => 'img/lot-4.jpg'
     ],
     [
-        'name'=>'Куртка для сноуборда DC Mutiny Charocal',
-        'cat'=>'Одежда',
-        'price'=>7500,
-        'url'=>'img/lot-5.jpg'
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'cat' => 'Одежда',
+        'price' => 7500,
+        'url' => 'img/lot-5.jpg'
     ],
     [
-        'name'=>'Маска Oakley Canopy',
-        'cat'=>'Разное',
-        'price'=>5400,
-        'url'=>'img/lot-6.jpg'
+        'name' => 'Маска Oakley Canopy',
+        'cat' => 'Разное',
+        'price' => 5400,
+        'url' => 'img/lot-6.jpg'
     ]
 ];
 function format_sum ($value)
 {
     $number = ceil($value);
-    if ($number <= 1000) {
-        $price = $number;
-    } else {
-        $price = number_format($number, 0, ',', " ");
-        return $price;
-    }
+    $price = number_format($number, 0, ',', " ");
+    return $price .= " &#8381";
 }
 ?>
 <!DOCTYPE html>
@@ -103,7 +99,7 @@ function format_sum ($value)
             <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
             <ul class="promo__list">
                 <!--заполните этот список из массива категорий-->
-                <?php foreach ($cats as $key=>$val):?>
+                <?php foreach ($categories as $key=>$val):?>
                     <li class="promo__item promo__item--boards">
                         <a class="promo__link" href="pages/all-lots.html"><?=$val?></a>
                     </li>
@@ -127,7 +123,7 @@ function format_sum ($value)
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Стартовая цена <?=$val['price']?> </span>
-                                    <span class="lot__cost">Цена <?=format_sum($val['price'])?><b class="rub">р</b></span>
+                                    <span class="lot__cost">Цена <?=format_sum($val['price'])?></span>
                                 </div>
                                 <div class="lot__timer timer">
                                     12:23
@@ -145,7 +141,7 @@ function format_sum ($value)
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($cats as $key=>$val):?>
+            <?php foreach ($categories as $key=>$val):?>
                 <li class="nav__item">
                     <a href="pages/all-lots.html"><?=$val?></a>
                 </li>
