79c7d023ce1f25b161ead7f7f05ef8f88b89eb12
Andrey <shevlyakov-a@mail.ru>
2019-04-29 21:42:09 +0300

    Сократили написание функции

--- functions/functions.php
+++ functions/functions.php
@@ -63,10 +63,12 @@ function get_link ()
 {
     $link = mysqli_connect("1055105-yeticave-9", "root", "","yeticave");
     mysqli_set_charset($link, "utf8");
-    if ($link == false){
-        print("Ошибка подключения: " .mysqli_connect_error());}
-    else return $link;
-    mysqli_close($link);
+    if ($link !== false){
+        return $link;
+    }
+        print("Ошибка подключения: " .mysqli_connect_error());
+        mysqli_close($link);
+
 }
 
 /*
@@ -74,17 +76,10 @@ function get_link ()
  */
 function get_categories () : array
 {
-        $link = get_link();
         $sql = "SELECT * FROM categories";
-        $result = mysqli_query($link, $sql);
-
-        if ($result) {
-            $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        } else {
-            $error = mysqli_error($link);
-            print("Ошибка MySQL: " . $error);
-        }
-    return $categories;
+        $result = mysqli_query(get_link(), $sql);
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $categories;
 }
 
 
@@ -93,11 +88,9 @@ function get_categories () : array
  */
 function get_lots() : array
 {
-    $link = get_link();
-    $sql_lots = 'SELECT img_url, category_id, l.name, price, c.NAME FROM lots l '
+    $sql = 'SELECT img_url, category_id, l.name, price, c.NAME FROM lots l '
         . 'JOIN categories c ON l.category_id = c.id';
-    if ($res = mysqli_query($link, $sql_lots)) {
-        $lots = mysqli_fetch_all($res, MYSQLI_ASSOC);
-    }
+    $result = mysqli_query(get_link(), $sql);
+    $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
     return $lots;
 }
