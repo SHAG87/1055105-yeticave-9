42a672e9aeb6ddf4b004538c63d137041a05b5cc
Andrey <shevlyakov-a@mail.ru>
2019-04-29 08:29:28 +0300

    SQL запросы перевёл в функции

--- functions/functions.php
+++ functions/functions.php
@@ -55,3 +55,48 @@ function last_hour (string $end_time) : bool
     $diff = $time_is_running_out - $now;
     return date('H', $diff) < 1;
 }
+
+/*
+ * Запрос на получение списка категорий
+ */
+function get_categories () : array
+{
+    $link = mysqli_connect("1055105-yeticave-9", "root", "","yeticave");
+    mysqli_set_charset($link, "utf8");
+    if ($link == false){
+        print("Ошибка подключения: " .mysqli_connect_error());}
+    else {
+        $sql = "SELECT * FROM categories";
+        $result = mysqli_query($link, $sql);
+
+        if ($result) {
+            $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        } else {
+            $error = mysqli_error($link);
+            print("Ошибка MySQL: " . $error);
+        }
+    }
+    return $categories;
+
+}
+
+
+/*
+ * Запрос на получение списка лотов (убрал пока условия, чтобы главная страница отображалась полностью, для наглядности)
+ */
+function get_lots() : array
+{
+    $link = mysqli_connect("1055105-yeticave-9", "root", "", "yeticave");
+    mysqli_set_charset($link, "utf8");
+    if ($link == false) {
+        print("Ошибка подключения: " . mysqli_connect_error());
+    } else {
+        $sql_lots = 'SELECT img_url, category_id, l.name, price, c.NAME FROM lots l '
+            . 'JOIN categories c ON l.category_id = c.id';
+        if ($res = mysqli_query($link, $sql_lots)) {
+            $lots = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        }
+    }
+    return $lots;
+
+}
--- index.php
+++ index.php
@@ -4,31 +4,8 @@ require_once ('functions\helpers.php');
 require_once ('functions\functions.php');
 require_once ('data.php');
 
-// Подключаем категории из БД
-$link = mysqli_connect("1055105-yeticave-9", "root", "","yeticave");
-mysqli_set_charset($link, "utf8");
-if ($link == false){
-    print("Ошибка подключения: " .mysqli_connect_error());
-}
-else {
-
-//Запрос на получение списка категорий
-    $sql = "SELECT * FROM categories";
-    $result = mysqli_query($link, $sql);
-
-    if ($result) {
-        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    } else {
-        $error = mysqli_error($link);
-        print("Ошибка MySQL: " .$error);
-    }
-//Запрос на получение списка лотов (убрал пока условия, чтобы главная страница отображалась полностью, для наглядности)
-    $sql_lots = 'SELECT img_url, category_id, l.name, price, c.NAME FROM lots l '
-                .'JOIN categories c ON l.category_id = c.id';
-    if ($res = mysqli_query($link, $sql_lots)) {
-        $lots =mysqli_fetch_all($res, MYSQLI_ASSOC);
-    }
-}
+$categories = get_categories();
+$lots = get_lots();
 
 $content = include_template('index.php', [
         'categories' => $categories,
